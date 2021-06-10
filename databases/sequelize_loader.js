184f77d87b0b94bc4e3bbc2ef1783a80900447a8
kashu-02 <hituji_shun@icloud.com>
2021-06-10 12:45:15 +0900

    Update sequelize_loader.js

--- databases/sequelize_loader.js
+++ databases/sequelize_loader.js
@@ -3,13 +3,30 @@ require('dotenv').config()
 
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+let sequelize
 
-const sequelize = new Sequelize(
+if(process.env.NODE_ENV == "development"){
+ sequelize = new Sequelize(
     process.env.DATABASE_URL,
     {
       operatorsAliases: true,
       logging: false,
-    });
+    })
+}else{
+ sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  {
+    operatorsAliases: true,
+    logging: false,
+    dialectOptions: {
+      useUTC: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  })
+}
 
 module.exports = {
   database: sequelize,
