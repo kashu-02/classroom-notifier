217e706c4ce0b03a79f8d2aba6a7e068a6422d68
kashu-02 <hituji_shun@icloud.com>
2021-06-10 12:50:46 +0900

    [skip ci] update cron.js

--- schedule/cron.js
+++ schedule/cron.js
@@ -1,24 +1,16 @@
 const CronJob = require('cron').CronJob;
 const chkclassroom = require('./chkclassroom')
 
-let starttime
-let endtime
-
 const workerJob = new CronJob({
   cronTime: '* * * * *',
   onTick: function () {
-    console.log("スタート")
     try {
-      starttime = process.hrtime()
       chkclassroom()
     } catch(e){
       console.log(e)
     }
   },
   onComplete: function () {
-    endtime = process.hrtime(starttime)
-    console.info('Execution time (hr): %ds %dms', endtime[0], endtime[1] / 1000000)
-    console.log('onComplete!')
   },
   start: true, //newした後即時実行するかどうか
   timeZone: 'Asia/Tokyo'
